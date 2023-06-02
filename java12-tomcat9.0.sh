a58610a43ee3f24212935c46bee6466590f7db3b
kishan7sep <67074550+kishan7sep@users.noreply.github.com>
2023-06-02 21:18:15 +0530

    Update java12-tomcat9.0.sh

--- java12-tomcat9.0.sh
+++ java12-tomcat9.0.sh
@@ -3,7 +3,7 @@ wget https://download.java.net/java/GA/jdk12.0.2/e482c34c86bd4bf8b56c0b35558996b
 tar -xvf openjdk-12.0.2_linux-x64_bin.tar.gz
 sudo mv jdk-12.0.2 /usr/lib/jvm/
 sudo ln -s /usr/lib/jvm/jdk-12.0.2/bin/java /usr/bin/java
-
+sudo chmod -R a+rwx /opt
 wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.75/bin/apache-tomcat-9.0.75.tar.gz
 tar -xvf apache-tomcat-9.0.75.tar.gz
 sudo mv apache-tomcat-9.0.75 /opt/tomcat9
