91d806a9a02b056391f4efdd1272d2b41f2749b1
kishan7sep <67074550+kishan7sep@users.noreply.github.com>
2023-06-02 21:14:07 +0530

    Create java12-tomcat9.0.sh

---
+++ java12-tomcat9.0.sh
@@ -0,0 +1,11 @@
+sudo yum update -y
+wget https://download.java.net/java/GA/jdk12.0.2/e482c34c86bd4bf8b56c0b35558996b9/10/GPL/openjdk-12.0.2_linux-x64_bin.tar.gz
+tar -xvf openjdk-12.0.2_linux-x64_bin.tar.gz
+sudo mv jdk-12.0.2 /usr/lib/jvm/
+sudo ln -s /usr/lib/jvm/jdk-12.0.2/bin/java /usr/bin/java
+
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.75/bin/apache-tomcat-9.0.75.tar.gz
+tar -xvf apache-tomcat-9.0.75.tar.gz
+sudo mv apache-tomcat-9.0.75 /opt/tomcat9
+echo "export CATALINA_HOME=/opt/tomcat9" >> ~/.bashrc
+source ~/.bashrc
