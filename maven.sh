fc82b37f2bab0c5246dfc635c8fb2ed9ff74afc2
kishan7sep <67074550+kishan7sep@users.noreply.github.com>
2023-06-05 10:15:46 +0530

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -1,5 +1,4 @@
-echo "Agent.ToolsDirectory: $Agent.ToolsDirectory"
-
-wget https://dlcdn.apache.org/maven/maven-3/3.8.8/binaries/apache-maven-3.8.8-bin.zip -o $1/apache-maven-3.8.8-bin.zip
+wget https://dlcdn.apache.org/maven/maven-3/3.8.8/binaries/apache-maven-3.8.8-bin.zip -o $1/
+ls $1
 unzip $1/apache-maven-3.8.8-bin.zip -d $1/
 $1/apache-maven-3.8.8/bin/mvn clean -verify "-Dcucumber.options=--tags @mycustomtag @blink, @blinkRegistration, @manageCliQ, @outwardCliQ, @payBillBlink, @requestMoneyBlink, @sendMoney"
