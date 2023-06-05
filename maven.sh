9cc3f6aadef048fad1a793401017fcb7177806a5
kishan7sep <67074550+kishan7sep@users.noreply.github.com>
2023-06-05 10:24:39 +0530

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -1,3 +1,4 @@
+rm -rvf $1/*maven*
 wget https://dlcdn.apache.org/maven/maven-3/3.8.8/binaries/apache-maven-3.8.8-bin.zip -P $1 -o apache-maven-3.8.8-bin.zip
 ls $1
 unzip $1/apache-maven-3.8.8-bin.zip -d $1/
