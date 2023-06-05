637495688368ce2a0ae662839cebfd366b5ce878
kishan7sep <67074550+kishan7sep@users.noreply.github.com>
2023-06-05 09:51:54 +0530

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -1,4 +1,3 @@
 wget https://dlcdn.apache.org/maven/maven-3/3.8.8/binaries/apache-maven-3.8.8-bin.zip
 unzip apache-maven-3.8.8-bin.zip
-cd apache-maven-3.8.8/bin
 ./apache-maven-3.8.8/bin/mvn clean -verify "-Dcucumber.options=--tags @mycustomtag @blink, @blinkRegistration, @manageCliQ, @outwardCliQ, @payBillBlink, @requestMoneyBlink, @sendMoney"
